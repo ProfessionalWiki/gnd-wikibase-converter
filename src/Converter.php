8b1e1d1a3b9708669faacdb4f3aad05741189d33
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-12-02 23:02:58 +0100

    Add exploratory ID spike

--- src/Converter.php
+++ src/Converter.php
@@ -6,4 +6,29 @@ namespace GND\WikibaseConverter;
 
 class Converter {
 
+	private const GND_ID = '007K';
+
+	/**
+	 * @psalm-pure
+	 * @param array{fields: array{array{name: string, subfields: array{array{name: string, value: string}}}}} $pica
+	 */
+	public function getIdFromPica( array $pica ): string {
+		foreach ( $pica['fields'] as $field ) {
+			if ( $field['name'] === self::GND_ID ) {
+				$properties = [];
+
+				foreach ( $field['subfields'] as $subfield ) {
+					$properties[$subfield['name']] = $subfield['value'];
+				}
+
+				if ( $properties['a'] === 'gnd' ) {
+					/** @psalm-suppress InvalidArrayOffset */
+					return $properties['0'];
+				}
+			}
+		}
+
+		return 'TODO';
+	}
+
 }
--- tests/Unit/ConverterTest.php
+++ tests/Unit/ConverterTest.php
@@ -4,10 +4,26 @@ declare( strict_types = 1 );
 
 namespace EDTF\Tests\Unit;
 
+use GND\WikibaseConverter\Converter;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * @covers \GND\WikibaseConverter\Converter
+ */
 class ConverterTest extends TestCase {
 
+	public function testGetId() {
+		$jsonString = file_get_contents( __DIR__ . '/../../data/GND-1-formatted.json' );
+		$pica = json_decode( $jsonString, true );
 
+		$this->assertSame(
+			'110-7',
+			$this->getId( $pica )
+		);
+	}
+
+	private function getId( array $pica ): string {
+		return ( new Converter() )->getIdFromPica( $pica );
+	}
 
 }
