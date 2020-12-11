1f4449cc317475a1c03c4336a36c62b741e67f76
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-12-11 23:24:18 +0100

    Remove spike code

--- src/PropertyMapping.php
+++ src/PropertyMapping.php
@@ -10,17 +10,10 @@ class PropertyMapping {
 		public /** @readonly */ string $propertyId,
 		public /** @readonly */ string $propertyType,
 		private /** @readonly string[] */ array $subfields,
-
-//		private ?SubfieldMatcher $picaSubfield = null,
 	) {}
 
 	public function shouldUseSubfieldValue( string $subfieldName ): bool {
 		return in_array( $subfieldName, $this->subfields );
 	}
-
-//	public string $picaFieldName;
-//	public string $picaSubfieldName;
-//	public string $picaSubfieldConditionName;
-//	public string $picaSubfieldConditionValue;
 
 }
