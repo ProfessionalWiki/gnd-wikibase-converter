503bff87195543e38398af95eee17f9aa571c2c3
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-12-19 22:20:59 +0100

    Make property private

--- src/PropertyMapping.php
+++ src/PropertyMapping.php
@@ -7,7 +7,7 @@ namespace DNB\WikibaseConverter;
 class PropertyMapping {
 
 	public function __construct(
-		public /** @readonly */ string $propertyId,
+		private /** @readonly */ string $propertyId,
 		private /** @readonly string[] */ array $subfields,
 		private /** @readonly */ ?SubfieldCondition $condition = null
 	) {}
