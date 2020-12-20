b2ae375a98905d121e13ae6037466acf80d0b0d3
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-12-20 17:45:24 +0100

    Add mapping deserialization smoke test

--- tests/Smoke/MappingJsonTest.php
+++ tests/Smoke/MappingJsonTest.php
@@ -5,6 +5,7 @@ declare( strict_types = 1 );
 namespace DNB\Tests\Smoke;
 
 use DNB\Tests\Files;
+use DNB\WikibaseConverter\MappingDeserializer;
 use PHPUnit\Framework\TestCase;
 
 class MappingJsonTest extends TestCase {
@@ -16,6 +17,15 @@ class MappingJsonTest extends TestCase {
 		$this->assertIsArray( $mapping );
 		$this->assertArrayHasKey( '007K', $mapping );
 		$this->assertIsArray( $mapping['007K'] );
+	}
+
+	/**
+	 * @depends testCanDeserialize
+	 */
+	public function testCanInstantiateObjects() {
+		$mapping = ( new MappingDeserializer )->jsonArrayToObject( json_decode( Files::getMappingJson(), true ) );
+
+		$this->assertNotEmpty( $mapping->getPropertyMappings( '003U' ) );
 	}
 
 }
