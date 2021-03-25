2f2111f8e37a9d1b26b2c90c5146b1240c77d171
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-03-25 16:58:23 +0100

    Extract in IdConverter

--- src/IdConverter.php
+++ src/IdConverter.php
@@ -9,13 +9,18 @@ class IdConverter {
 	public function gndToWikibaseId( string $gndId ): string {
 		if ( preg_match( '/1[012]?\d{7}[0-9X]/', $gndId ) === 1 ) {
 			if ( str_ends_with( $gndId, 'X' ) ) {
-				return preg_replace('/[^0-9]/', '', $gndId ) . '02';
+				return $this->transformGndId( $gndId, 2 );
 			}
 
-			return $gndId . '01';
+			return $this->transformGndId( $gndId, 1 );
 		}
 
+		return 'TODO';
+	}
 
+	private function transformGndId( string $gndId, int $caseNumber ): string {
+		return preg_replace('/[^0-9]/', '', $gndId )
+			. str_pad( (string)$caseNumber, 2, '0', STR_PAD_LEFT );
 	}
 
 }
