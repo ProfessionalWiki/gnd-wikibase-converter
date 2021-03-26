82cc6bf8b32e86cf0551a40f11dff896043459f7
Jeroen De Dauw <jeroendedauw@gmail.com>
2021-03-26 17:26:31 +0100

    Added numericIdToGnd

--- src/IdConverter.php
+++ src/IdConverter.php
@@ -18,24 +18,49 @@ class IdConverter {
 	 * Format 2: numeric ID with a dash: 2 is appended. ie 12-3 -> 1232
 	 * Format 3: ID ends on -X: 3 is appended. ie 12-X -> 123
 	 */
-	public function gndToNumericId( string $gndId ): string {
+	public function gndToNumericId( string $gndId ): int {
 		if ( ctype_digit( $gndId ) ) {
-			return $gndId . '0';
+			return (int)( $gndId . '0' );
 		}
 
 		if ( str_ends_with( $gndId, '-X' ) ) {
-			return preg_replace('/[^0-9]/', '', $gndId ) . '3';
+			return (int)( $this->removeNonNumbers( $gndId ) . '3' );
 		}
 
 		if ( str_ends_with( $gndId, 'X' ) ) {
-			return preg_replace('/[^0-9]/', '', $gndId ) . '1';
+			return (int)( $this->removeNonNumbers( $gndId ) . '1' );
 		}
 
 		if ( substr( $gndId, -2, 1 ) === '-' ) {
-			return preg_replace('/[^0-9]/', '', $gndId ) . '2';
+			return (int)( $this->removeNonNumbers( $gndId ) . '2' );
 		}
 
 		throw new \InvalidArgumentException( 'Invalid GND ID' );
+	}
+
+	private function removeNonNumbers( string $string ): string {
+		return preg_replace('/[^0-9]/', '', $string );
+	}
+
+	/**
+	 * Reverse of @see gndToNumericId
+	 */
+	public function numericIdToGnd( int $numericId ): string {
+		$idFormat = substr( (string)$numericId, -1, 1 );
+		$idWithoutFormat = substr( (string)$numericId, 0, -1 );
+
+		switch ( $idFormat ) {
+			case '0':
+				return $idWithoutFormat;
+			case '1':
+				return $idWithoutFormat . 'X';
+			case '2':
+				return substr( $idWithoutFormat, 0, -1 ) . '-' . substr( $idWithoutFormat, -1, 1 );
+			case '3':
+				return $idWithoutFormat . '-X';
+			default:
+				throw new \InvalidArgumentException( 'Invalid numeric GND ID' );
+		}
 	}
 
 }
--- tests/Unit/IdConverterTest.php
+++ tests/Unit/IdConverterTest.php
@@ -15,7 +15,7 @@ class IdConverterTest extends TestCase {
 	/**
 	 * @dataProvider transformationProvider
 	 */
-	public function testFoo( string $gndId, string $transformedId ): void {
+	public function testGndToNumericId( string $gndId, int $transformedId ): void {
 		$this->assertSame(
 			$transformedId,
 			( new IdConverter() )->gndToNumericId( $gndId )
@@ -23,20 +23,35 @@ class IdConverterTest extends TestCase {
 	}
 
 	public function transformationProvider(): \Generator {
-		yield 'Case 0: all numeric' => [ '123', '1230' ];
-		yield 'Case 1: ends on X' => [ '123X', '1231' ];
-		yield 'Case 2: second last is dash' => [ '12-3', '1232' ];
-		yield 'Case 3: ends on -X' => [ '12-X', '123' ];
+		yield 'Case 0: all numeric' => [ '123', 1230 ];
+		yield 'Case 1: ends on X' => [ '123X', 1231 ];
+		yield 'Case 2: second last is dash' => [ '12-3', 1232 ];
+		yield 'Case 3: ends on -X' => [ '12-X', 123 ];
 
-		yield [ '110-7', '11072' ];
-		yield [ '101115658X', '1011156581' ];
-		yield [ '1033476056', '10334760560' ];
-		yield [ '1324-X', '13243' ];
+		yield [ '110-7', 11072 ];
+		yield [ '101115658X', 1011156581 ];
+		yield [ '1033476056', 10334760560 ];
+		yield [ '1324-X', 13243 ];
 	}
 
 	public function testThrowsExceptionOnInvalidGndId(): void {
 		$this->expectException( \InvalidArgumentException::class );
 		( new IdConverter() )->gndToNumericId( '~=[,,_,,]:3' );
+	}
+
+	/**
+	 * @dataProvider transformationProvider
+	 */
+	public function testNumericIdToGnd( string $gndId, int $transformedId ): void {
+		$this->assertSame(
+			$gndId,
+			( new IdConverter() )->numericIdToGnd( $transformedId )
+		);
+	}
+
+	public function testThrowsExceptionOnInvalidNumericGndId(): void {
+		$this->expectException( \InvalidArgumentException::class );
+		( new IdConverter() )->numericIdToGnd( 1337 );
 	}
 
 }
